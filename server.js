f38b14e8657ab625ffdcb42ff281c23b51fe4182
Cody Brooks <codyjamesbrooks@gmail.com>
2021-10-14 14:01:04 -0600

    inital project setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+const webpack = require("webpack");
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpackConfig = require("./webpack.config");
+
+const app = express()
+const distPath = path.join(__dirname, "dist");
+const port = process.env.PORT || 3000
+
+app.use(express.static(distPath))
+app.use(webpackMiddleware(webpack(webpackConfig)))
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
