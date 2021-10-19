79becc85e814e9d1d5e01e15459183ed37359576
Cody Brooks <codyjamesbrooks@gmail.com>
2021-10-19 10:17:54 -0600

    rename controller name for added clarity, add phone_number.js for help with validating/formatting/parsing phone numbers

---
+++ src/controllers/phone_link_controller.js
@@ -0,0 +1,100 @@
+import { Controller } from "@hotwired/stimulus";
+import { phoneNumberObject as phoneNumber } from "../phone_number";
+
+export default class extends Controller {
+  static values = { escape: String, 
+                    country: String,
+                    area: String,
+                    tel: String, 
+                    line: String, }
+  static classes = [ 'hidden' ]
+  static targets = [ 'number', 'message', 'generatedLink', 'numberInfo', 
+                     'escape', 'country', 'area', 'tel', 'line'];
+  
+                     
+  get number() {
+    return this.numberTarget.value.replace(/[^\d\+]/g, '')
+  }
+  get numberInfo() {
+    return this.numberInfoTarget;
+  }
+  get message() {
+    return this.messageTarget;
+  }
+  get generatedLink() {
+   return this.generatedLinkTarget;
+  }
+
+  hideElement(...elements) {
+    elements.forEach((element) => element.classList.add(this.hiddenClass))
+  }
+  revalElements(...elements) {
+    elements.forEach((element) => element.classList.remove(this.hiddenClass))
+  }
+
+  telValueChanged(value) {
+    this.telTarget.innerHTML = `Tel Prefix:   ${value}`
+  }
+  lineValueChanged(value) {
+    this.lineTarget.innerHTML = `Line Number:   ${value}`
+  }
+  areaValueChanged(value) {
+    this.areaTarget.innerHTML = `Area Code: ${value}`
+  }
+  countryValueChanged(value) {
+    this.countryTarget.innerHTML = `Country Code: ${value}`
+  }
+  escapeValueChanged(value) {
+    this.escapeTarget.innerHTML = `Escape Code: ${value}`
+  }
+
+
+  createNumber() {
+    return new phoneNumber(this.number)
+  }
+
+  change() {
+    let potentialNumber = this.createNumber()
+    this.updateMessageDisplay()
+
+    if (potentialNumber.valid()) {
+      // get & set the phone number info
+      let phoneInfo = potentialNumber.parseNumber()
+      this.setPhoneNumberInformation(phoneInfo)
+      
+      // create the phone link
+      let formattedPhoneNumber = potentialNumber.format()
+      this.createPhoneLink(formattedPhoneNumber)
+
+      this.revalElements(this.generatedLink, this.numberInfo)
+    } else {
+      // Phone number isn't valid yet. 
+      this.hideElement(this.generatedLink, this.numberInfo)
+    }
+  }
+
+  setPhoneNumberInformation(phoneInfo) {
+    if (phoneInfo.lineNumber != this.lineValue) { this.lineValue= phoneInfo.lineNumber }
+    if (phoneInfo.telPrefix!= this.telValue) { this.telValue= phoneInfo.telPrefix}
+    if (phoneInfo.areaCode!= this.areaValue) { this.areaValue= phoneInfo.areaCode }
+    if (phoneInfo.countryCode!= this.countryValue) { this.countryValue = phoneInfo.countryCode }
+    if (phoneInfo.escapeCode!= this.escapeValue) { this.escapeValue= phoneInfo.escapeCode }
+  }
+
+  updateMessageDisplay() {
+    if (this.number.length === 0) {
+      this.message.innerHTML = "Enter phone number"
+    } else if (this.number.length <= 6) {
+      this.message.innerHTML = "Continue to input the phone number"
+    } else if (this.number.length <= 12) {
+      this.message.innerHTML = "That's a start, lets see what you got"
+    } else if (this.number.length === 13) {
+      this.message.interHTML = "One heck of a link you got there"
+    }
+  }
+
+  createPhoneLink(fomattedPhoneNumber) {
+    this.generatedLink.setAttribute("href", `tel:${this.number}`);
+    this.generatedLink.innerHTML = fomattedPhoneNumber
+  }
+}
---
+++ src/phone_number.js
@@ -0,0 +1,45 @@
+function phoneNumberObject(number) {
+  this.number = number
+  this.phoneREGEX = new RegExp(/^(\+)?(\d{1,2})?\s?(\(?(\d{3})\)?)?[\s.-]?(\d{3})[\s.-]?(\d{4})$/)
+
+  this.valid = function() {
+    return this.phoneREGEX.test(this.number)
+  }
+
+  this.parseNumber = function() {
+    let matchGroups = this.number.match(this.phoneREGEX)
+    let parts = {}
+    if (matchGroups) {
+      parts.escapeCode = matchGroups[1] ?? "None Provided",
+      parts.countryCode = matchGroups[2] ?? "None Provided",
+      parts.areaCode = matchGroups[4] ?? "None Provided",
+      parts.telPrefix = matchGroups[5] ?? "None Provided",
+      parts.lineNumber = matchGroups[6] ?? "None Provided"
+    }
+    return parts;
+  }
+
+  this.format = function() {
+    let phoneParts = this.parseNumber()
+    let formatedNumber = ""
+    formatedNumber += this.formatEscapeAndCountryCode(phoneParts)
+    if (phoneParts.areaCode != "None Provided") {
+      formatedNumber += `(${phoneParts.areaCode}) `
+    }
+    formatedNumber += `${phoneParts.telPrefix}-${phoneParts.lineNumber}`
+    return formatedNumber
+  }
+
+  this.formatEscapeAndCountryCode= function(parsedPhone) {
+    let escape = parsedPhone.escapeCode != "None Provided" ? parsedPhone.escapeCode : ""
+    let country = parsedPhone.countryCode != "None Provided" ? parsedPhone.countryCode : ""
+    if (escape && country) {
+      return `${escape}${country} `
+    } else if (country) {
+      return `${country} `
+    } 
+    return escape
+  }
+}
+
+export { phoneNumberObject }
