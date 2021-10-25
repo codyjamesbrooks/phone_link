4adbe13ac48ebb5a2e72763962d3e52717972af4
Cody Brooks <codyjamesbrooks@gmail.com>
2021-10-25 15:31:45 -0600

    change phone-number input style, adjusted edu content for exit/country/phone

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const webpackConfig = require("./webpack.config")
 
 const app = express()
 const publicPath = path.join(__dirname, "public");
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 3000
 
 app.use(express.static(publicPath))
 app.use(webpackMiddleware(webpack(webpackConfig)))
