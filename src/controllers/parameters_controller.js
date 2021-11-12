79b85b31f94aaaedbb9005eeb52271d53509b63f
Cody Brooks <codyjamesbrooks@gmail.com>
2021-11-11 22:44:00 -0700

    copy to clipboard preview

--- src/controllers/parameters_controller.js
+++ src/controllers/parameters_controller.js
@@ -1,22 +1,27 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = [ 'output', 'callLink' ]
+  static targets = [ 'output', 'callLink', 'embedPreview' ]
 
   initialize() {
     let params = new URLSearchParams(document.location.search)
     this.number = params.get('number')
-    this.setOutputHTML()
-    this.setLink()
+    this.updateOutputHTML()
+    this.updateLink()
+    this.updateEmbedPreview()
   }
 
-  setOutputHTML() {
+  updateOutputHTML() {
     this.outputTarget.innerHTML = this.number
   }
 
-  setLink() {
+  updateLink() {
     this.callLinkTarget.innerHTML = this.number
     let phoneHref = `tel:+1${this.number.replace(/[^+\d]/g, "")}`
     this.callLinkTarget.setAttribute("href", phoneHref)
+  }
+
+  updateEmbedPreview() {
+    this.embedPreviewTarget.innerHTML = `+1${this.number.replace(/[^+\d]/g, "")}`
   }
 }
